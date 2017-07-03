ffceff34fd7af0b75c0e58705c9f048520f807b4
Varghese Chacko <varghesec@gmail.com>
2017-07-03 11:04:34 +0530

    Fix setup file

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name='Atemon-EmailValidator',
-    version='0.1.1.3',
+    version='0.1.2.0',
     packages=['atemon', 'atemon.EmailValidator'],
     long_description="Check if the given email address is valid or not using nslookup",
     author="Varghese Chacko",
     author_email="varghese@atemon.com",
-    url="https://github.com/atemon/python-sms-api",
+    url="https://github.com/atemon/Python-EmailValidator",
     provides=["EmailValidator"],
     license="MIT License",
 )
